1c67b55afe40560801b3d1f861d5edbdf094eb53
Jane Chou <chouchiachen1@gmail.com>
2021-09-06 12:11:51 +0800

    add header

--- products.py
+++ products.py
@@ -7,8 +7,15 @@ while True:
 	p = [name, price]
 	products.append(p)
 print(products)
-
 print(products[1][0])
+
 
 for product in products:
 	print(product[0], '的價格是', product[1])
+
+with open('products.csv', 'w', encoding='utf-8') as f:  # with 檔案讀取
+		f.write('商品,價格\n')
+		for p in products:
+			f.write(p[0] + ',' + p[1] + '\n') # ,做分隔 \n換行
+
+
